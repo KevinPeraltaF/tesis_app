2f733184e49bc8ec07c073b573645c36efec02b3
Kevin <karellanou@unemi.edu.ec>
2022-06-13 23:17:15 -0500

    login

--- tesis/urls.py
+++ tesis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from tesis.views import dashboard
+from tesis.views import dashboard,login
 
 urlpatterns = [
+path(r'login', login, name='login'),
     path(r'', dashboard, name='dashboard'),
 
 ]
--- tesis/views.py
+++ tesis/views.py
@@ -22,3 +22,22 @@ def dashboard(request):
                 return render(request, "registration/dashboard.html ", data)
             except Exception as ex:
                 print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+
+def login(request):
+    global ex
+    data = {}
+    if request.method == 'POST':
+        if 'peticion' in request.POST:
+            peticion = request.POST['peticion']
+    else:
+        if 'peticion' in request.GET:
+            peticion = request.GET['peticion']
+
+        else:
+            try:
+                data['titulo'] = 'Inicio de Sesion'
+
+                return render(request, "registration/login.html ", data)
+            except Exception as ex:
+                print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+

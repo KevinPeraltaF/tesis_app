a1f519ad4468837e5fc7bd21ea24b6801a586f0f
KevinPeraltaF <micontrasea99>
2022-06-15 23:06:09 -0500

    login

--- tesis/urls.py
+++ tesis/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from tesis.view_clase import Clase
-from tesis.views import Dashboard, Login
+from tesis.views import Dashboard, Login, Logout
 
 urlpatterns = [
-    path(r'login', Login, name='login'),
-    path(r'clase', Clase, name='clase'),
     path(r'', Dashboard, name='dashboard'),
+    path(r'login/', Login, name='login'),
+    path(r'logout/', Logout, name='logout'),
+    path(r'clase/', Clase, name='clase'),
+
 
 ]
--- tesis/views.py
+++ tesis/views.py
@@ -1,8 +1,9 @@
 import sys
 
+from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,7 +12,8 @@ from django.template.loader import get_template
 from tesis.forms import ClaseForm
 from tesis.models import Clase
 
-
+@login_required(redirect_field_name='next', login_url='/login')
+@transaction.atomic()
 def Dashboard(request):
     global ex
     data = {}
@@ -91,21 +93,42 @@ def Dashboard(request):
             except Exception as ex:
                 print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
 
+@transaction.atomic()
 def Login(request):
     global ex
     data = {}
     if request.method == 'POST':
         if 'peticion' in request.POST:
             peticion = request.POST['peticion']
+            if peticion == 'login_usuario':
+                try:
+                    usuario = authenticate(username=request.POST['usuario'].lower().strip(), password=request.POST['clave'])
+                    if usuario is not None:
+                        if usuario.is_active:
+                            login(request, usuario)
+                            return JsonResponse({"respuesta": True})
+                        else:
+                            return JsonResponse({"respuesta": False, 'mensaje': u'Inicio de sesión incorrecto, usuario no activo.'})
+                    else:
+                        return JsonResponse({"respuesta": False,'mensaje': u'Inicio de sesión incorrecto, usuario o clave no coinciden.'})
+                except Exception as ex:
+                    transaction.set_rollback(True)
+                    return JsonResponse({"respuesta": False, "mensaje": "Error al iniciar sesión, intentelo más tarde."})
+        return JsonResponse({"respuesta": False, "mensaje": "acción Incorrecta."})
     else:
         if 'peticion' in request.GET:
             peticion = request.GET['peticion']
-
         else:
             try:
-                data['titulo'] = 'Inicio de Sesion'
-
-                return render(request, "registration/login.html ", data)
+                if 'persona' in request.session:
+                    return HttpResponseRedirect("/")
+                data['titulo'] = 'Inicio de sesión'
+                data['request'] = request
+                return render(request, "registration/login.html", data)
             except Exception as ex:
                 print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
 
+
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect("/login")

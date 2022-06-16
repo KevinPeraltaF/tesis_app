21760b87796a700d8f012a8b4f6850808d24c08c
KevinPeraltaF <micontrasea99>
2022-06-15 22:30:38 -0500

    crear clase

--- tesis/models.py
+++ tesis/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class ModeloBase(models.Model):
+    from django.contrib.auth.models import User
+    usuario_creacion = models.ForeignKey(User, verbose_name='Usuario Creación', blank=True, null=True, on_delete= models.CASCADE, related_name='+', editable=False)
+    fecha_creacion = models.DateTimeField(verbose_name='Fecha creación',auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(verbose_name='Fecha Modificación', auto_now=True)
+    usuario_modificacion = models.ForeignKey(User, verbose_name='Usuario Modificación', blank=True, null=True, on_delete= models.CASCADE, related_name='+', editable=False)
+    status = models.BooleanField(verbose_name="Estado del registro", default=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        usuario = None
+        if len(args):
+            usuario = args[0].user.id
+        if self.id:
+            self.usuario_modificacion_id = usuario
+        else:
+            self.usuario_creacion_id = usuario
+        models.Model.save(self)
+
+
+class Clase(ModeloBase):
+    nombre = models.CharField(verbose_name="Nombre de la clase", max_length=100)
+    seccion =models.CharField(verbose_name="Sección", max_length=100)
+    materia =models.CharField(verbose_name="Materia", max_length=100)
+    aula =models.CharField(verbose_name="Aula", max_length=100)
+    archivada = models.BooleanField(default=False, verbose_name=u'Archivada')
+    class Meta:
+        verbose_name = "Clase"
+        verbose_name_plural = "Clases"
+        ordering = ['id']
+
+
+    def __str__(self):
+        return u'%s' % self.nombre
--- tesis/views.py
+++ tesis/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 from tesis.forms import ClaseForm
+from tesis.models import Clase
 
 
 def Dashboard(request):
@@ -17,6 +18,56 @@ def Dashboard(request):
     if request.method == 'POST':
         if 'peticion' in request.POST:
             peticion = request.POST['peticion']
+            if peticion == 'crear_clase':
+                try:
+                    form = ClaseForm(request.POST, request.FILES)
+                    if form.is_valid():
+                        nombre = form.cleaned_data['nombre']
+                        seccion = form.cleaned_data['seccion']
+                        materia = form.cleaned_data['materia']
+                        aula = form.cleaned_data['aula']
+
+                        clase = Clase(
+                            nombre=nombre,
+                            seccion=seccion,
+                            materia=materia,
+                            aula=aula,
+                            archivada=False
+
+                        )
+                        clase.save(request)
+
+                        return JsonResponse({"result": True}, safe=False)
+                    else:
+                        return JsonResponse( {"respuesta": False, "mensaje": "Ha ocurrido un error al enviar los datos."})
+
+
+                except Exception as ex:
+                    transaction.set_rollback(True)
+                    return JsonResponse({"respuesta": False, "mensaje": "Ha ocurrido un error, intente mas tarde."})
+
+            if peticion == 'crear_clase':
+                try:
+                    form = ClaseForm(request.POST, request.FILES)
+                    if form.is_valid():
+                        nombre = form.cleaned_data['nombre']
+                        seccion = form.cleaned_data['seccion']
+                        materia = form.cleaned_data['materia']
+                        aula = form.cleaned_data['aula']
+                        clase = Clase(
+                            nombre=nombre,
+                            seccion=seccion,
+                            materia=materia,
+                            aula=aula,
+                            archivada=False
+
+                        )
+                        clase.save(request)
+                        return JsonResponse({"result": False}, safe=False)
+
+                except Exception as ex:
+                    transaction.set_rollback(True)
+                    return JsonResponse({"result": True, "mensaje": "Datos erróneos, intente nuevamente."}, safe=False)
     else:
         if 'peticion' in request.GET:
             peticion = request.GET['peticion']
@@ -25,6 +76,7 @@ def Dashboard(request):
                 try:
                     form = ClaseForm()
                     data['form'] = form
+                    data['peticion'] = 'crear_clase'
                     template = get_template("clase/crear_clase.html")
                     return JsonResponse({"respuesta": True, 'data': template.render(data)})
                 except Exception as ex:

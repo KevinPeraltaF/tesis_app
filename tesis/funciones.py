0697fd15211b7467d751af1d60564f4502a1ed3d
KevinPeraltaF <micontrasea99>
2022-06-17 23:34:28 -0500

    crear usuario

--- tesis/funciones.py
+++ tesis/funciones.py
@@ -1,3 +1,3 @@
 def Data_inicial(request,data):
     from tesis.models import Clase
-    data['clases'] = clases = Clase.objects.filter(status=True, archivada=False)
+    data['clases'] = clases = Clase.objects.filter(status=True, archivada=False,usuario_creacion = request.user)

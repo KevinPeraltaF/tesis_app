f355b562955e3af971f2319bdf15624bcbd3722c
KevinPeraltaF <micontrasea99>
2022-08-29 20:50:32 -0500

    promedio

--- tesis/forms.py
+++ tesis/forms.py
@@ -154,7 +154,7 @@ class CrearVideoForm(FormularioGeneralPublicacion):
 
 class SubirTareaForm(forms.Form):
     archivo = forms.FileField(label='Subir Tarea', required=True, widget=forms.ClearableFileInput(
-        attrs={'class': 'dropify', 'data-allowed-file-extensions': 'pdf doc docx'}))
+        attrs={'class': 'dropify', 'data-allowed-file-extensions': 'pdf doc docx pptx ppt'}))
 
 
 
